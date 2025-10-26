12bfb62a4a9858b6382f140eee348ea9fabf112a
v4to <andrew.egorcev@gmail.com>
2025-10-26 15:33:24 +0200

    feat: implement roundToPowerOfTen function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -177,8 +177,10 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const powerOfTen = 10 ** pow;
+
+  return Math.round(num / powerOfTen) * powerOfTen;
 }
 
 /**
