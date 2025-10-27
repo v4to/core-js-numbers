1050510b6c5244eb6bfe2e89992d2890ea767557
v4to <andrew.egorcev@gmail.com>
2025-10-27 12:57:04 +0200

    feat: implement getSumOfNumbers function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -583,8 +583,10 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  const sum = x1 + x2 + x3;
+
+  return Number(sum.toFixed(1));
 }
 
 /**
